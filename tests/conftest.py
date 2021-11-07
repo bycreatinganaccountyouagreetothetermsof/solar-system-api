7f94535a7ae6ecdcef4dc2073837896cec2dc957
lain <bycreatinganaccountyouagreetothetermsof@users.noreply.github.com>
2021-11-07 11:23:26 -0800

    initial tests and fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,37 @@
+import pytest
+from app import create_app
+from app import db
+from app.models.planet import Planet
+
+
+@pytest.fixture
+def app():
+    app = create_app({"TESTING": True})
+
+    with app.app_context():
+        db.create_all()
+        yield app
+
+    with app.app_context():
+        db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def eight_planets(app):
+    eight_planets = [
+        Planet(name="mercury", description="the littlest planet", color="gray"),
+        Planet(name="venus", description="the hottest planet", color="maroon"),
+        Planet(name="earth", description="the liveliest planet", color="green"),
+        Planet(name="mars", description="the reddest planet", color="red"),
+        Planet(name="jupiter", description="the biggest planet", color="orange"),
+        Planet(name="saturn", description="the ring planet", color="yellow"),
+        Planet(name="uranus", description="the most sidways planet", color="purple"),
+        Planet(name="neptune", description="the boring planet", color="blue"),
+    ]
+    db.session.add_all(eight_planets)
+    db.session.commit()
---
+++ tests/test_routes.py
@@ -0,0 +1,19 @@
+def test_get_all_planets_with_no_records(client):
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == []
+
+
+def test_get_one_planet(client, eight_planets):
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "name": "mercury",
+        "description": "the littlest planet",
+        "color": "gray",
+    }
